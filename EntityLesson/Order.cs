d60355abf57adceaee76976e38bc67fd7e61de85
Chingis B. Sanzhiev <cbs1@tpu.ru>
2021-08-27 14:02:09 +0700

    Добавьте файлы проекта.

---
+++ EntityLesson/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EntityLesson
+{
+    public partial class Order
+    {
+        public int OrderId { get; set; }
+        public string User { get; set; }
+        public string Address { get; set; }
+        public string ContactPhone { get; set; }
+        public int PhoneId { get; set; }
+
+        public virtual Phone Phone { get; set; }
+    }
+}
---
+++ EntityLesson/Phone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EntityLesson
+{
+    public partial class Phone
+    {
+        public Phone()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public int Price { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
